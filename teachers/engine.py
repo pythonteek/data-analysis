e347bb5addc557221b01cc1ee08eaba9ea24376f
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-19 12:26:45 +0330

    test class and head

--- teachers/engine.py
+++ teachers/engine.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('datasets/teachers.csv')
+class Teachers:
+    def __init__(self):
+        self.teacher_frame = pd.read_csv('datasets/teachers.csv')
 
+    def get_head(self):
+        return self.teacher_frame.head()
 
+T1 = Teachers()
+print(T1.get_head())
