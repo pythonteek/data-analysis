f1df7e9bab11dfb4cff1d08e12f2e10719dbe67d
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2024-03-19 12:36:20 +0330

    add_jalali_date_to_teacher_frame

--- teachers/engine.py
+++ teachers/engine.py
@@ -12,6 +12,10 @@ class Teachers:
     def get_date_created(self):
         return self.teacher_frame['date_created']
 
+    def add_jalali_date_to_teacher_frame(self):
+        date_create = self.get_date_created()
+        print(date_create)
+
 #print(JalaliDate.to_jalali(2013, 9, 16))
 T1 = Teachers()
-print(T1.get_date_created())
+T1.add_jalali_date_to_teacher_frame()
